5c8b99be149cd7193d6bc35ab72f49ef8bf37339
nghiavt <nghiavt@sandinh.net>
2025-03-21 17:58:24 +0700

    update properties
     - Setup docker and local
     - Add TestController

--- src/main/java/com/example/online_auction_platform/controllers/TestController.java
+++ src/main/java/com/example/online_auction_platform/controllers/TestController.java
@@ -3,9 +3,11 @@ package com.example.online_auction_platform.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.online_auction_platform.entities.Bidder;
 
 @RestController
 public class TestController {
-
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
